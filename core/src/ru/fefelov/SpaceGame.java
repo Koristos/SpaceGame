080f4dfc5d399b2ecc234b822f4002faf2182317
Fefelov <fefelov.sa@yandex.ru>
2021-11-20 18:14:15 +0300

    HomeWorkOne

--- core/src/ru/fefelov/SpaceGame.java
+++ core/src/ru/fefelov/SpaceGame.java
@@ -1,6 +1,8 @@
 package ru.fefelov;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,14 +14,14 @@ public class SpaceGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("background.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
