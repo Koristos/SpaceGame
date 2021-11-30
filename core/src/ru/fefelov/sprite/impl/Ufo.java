a0e11e8b3220399316c2a92122ad8df9c54ac417
fefelov.sergei <fefelov.sergei@otr.ru>
2021-12-01 01:05:01 +0300

    LessonFour

--- core/src/ru/fefelov/sprite/impl/Ufo.java
+++ core/src/ru/fefelov/sprite/impl/Ufo.java
@@ -10,28 +10,36 @@ import ru.fefelov.sprite.Sprite;
 
 public class Ufo extends Sprite {
 
+    private final float SPEED = 0.005f;
+    private final float HEIGHT = 0.2f;
+
     private Vector2 destination = new Vector2();
     static Vector2 move = new Vector2();
-    static final float SPEED = 0.005f;
+    private boolean enabled;
 
-    public Ufo(Texture texture) {
+
+    public Ufo(Texture texture, boolean isEnabled) {
         super(new TextureRegion(texture));
+        this.enabled = isEnabled;
     }
 
     @Override
     public void resize(Rect worldBounds) {
-        setHeightProportion(worldBounds.getHeight()*0.1f);
+        setHeightProportion(worldBounds.getHeight()*HEIGHT);
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        if (!enabled) return false;
         this.destination.set(touch);
         return super.touchDown(touch, pointer, button);
     }
 
     @Override
     public void draw(SpriteBatch batch) {
-        calculateMovement(this.pos, this.destination);
+        if (enabled) {
+            calculateMovement(this.pos, this.destination);
+        }
         super.draw(batch);
     }
 
